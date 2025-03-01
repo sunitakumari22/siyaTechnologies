855e2eabb73932c0127ac459399eebe03c2dfb50
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-01 15:16:47 +0530

    added bootstrap

--- siyatech/src/App.jsx
+++ siyatech/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar';
+import Login from './components/login';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <Navbar title="SiyaTech" />
+    <Login></Login>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +31,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
     </>
   )
 }
