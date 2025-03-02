ec3d01644b0f02cddfd1d3ee269949f1181a3b87
sunita kumari <139567148+SunitaKumari2004@users.noreply.github.com>
2025-03-02 22:17:22 +0530

    routing changes

--- siyatech/src/App.jsx
+++ siyatech/src/App.jsx
@@ -4,15 +4,24 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Home from './components/home'
 import Signup from './components/signup';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/contact';
+import Gallary from './components/Gallary';
 
 function App() {
   return (
     <Router>
       {/* <Navbar title="SiyaTech" /> */}
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navbar />} />
+        <Route index element={<Home />} />  {/* Default home page */}
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallary" element={<Gallary />} />
       </Routes>
     </Router>
   );
